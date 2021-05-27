f8e779c2cedd9553779f956ea36f19c0873afc08
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2021-05-27 14:23:56 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading'
+//Vamos criar isso, pq ao inves de chamar tela por tela, chamamos o Routes
+import Routes from './src/routes' 
+import{
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+}from '@expo-google-fonts/jost'
 
 export default function App() {
+//Vamos arazenar o retorno do Boolean dentro do fontsLoaded
+  const [ fontsLoaded ] = useFonts({
+    //Vamos passar um objeto para o nosso useFonts e vamos incluir as fontes que vamos usar
+    Jost_400Regular,
+    Jost_600SemiBold
+  });
+//Vamos poder utilizar ele dentro de uma condição
+//Agora a condição vai testar, ela está carregada? ela já crregou?
+if(!fontsLoaded)
+return <AppLoading/>
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Routes/> //Ele vai chamar as telas
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
