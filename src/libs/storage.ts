23bdf4d79588370f367858259656c47617bb37d4
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2021-05-27 15:05:02 -0300

    Update storage.ts

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -16,7 +16,7 @@ export interface PlantProps{
         dateTimeNotification: Date;
 }
 
-interface StoragePlantsProps {
+export interface StoragePlantsProps {
     [id: string]: {
         data: PlantProps;
     }
