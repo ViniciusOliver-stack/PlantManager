481023b98c0b6dc4cf48e7e0b3bd0b84214ae24f
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2021-05-27 14:24:27 -0300

    Create fonts.ts

---
+++ src/styles/fonts.ts
@@ -0,0 +1,8 @@
+//Centralizar em um arquivo que fica mais fácil fazer a alteração
+export default{
+    heading: 'Jost_600SemiBold',
+    text: 'Jost_400Regular',
+    complement: 'Jost_400Regular'
+}
+
+    
