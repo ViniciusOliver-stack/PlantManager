d474f594697db0dcd1a0e7861763a0376ae7f333
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2021-06-16 22:14:11 -0300

    Update PlantCardSecundary.tsx

--- src/components/PlantCardSecundary.tsx
+++ src/components/PlantCardSecundary.tsx
@@ -2,26 +2,41 @@ import React from 'react'
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    Animated
 } from 'react-native'
 import {SvgFromUri} from 'react-native-svg'
 import {RectButton, RectButtonProps} from 'react-native-gesture-handler'
 import Swipeable from 'react-native-gesture-handler/Swipeable'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
+import { Feather } from '@expo/vector-icons'
 
 interface PlantProps extends RectButtonProps{
     data:{
         name: string;
         photo: string;
         hour: string;
-    }
+    };
+    handleRemove: () => void;
 }
 
 export const PlantCardSecundary= ({ data, ...rest} : PlantProps) =>{
     return(
         <Swipeable
             overshootRight={false}
+            renderRightActions={()=>(
+                <Animated.View>
+                    <View>
+                        <RectButton 
+                            style={styles.buttonRemove}
+                            onPress={handleRemove}
+                        >
+                            <Feather name="trash" size={32} color={colors.white}/>
+                        </RectButton>
+                    </View>
+                </Animated.View>
+            )}
         >
         <RectButton
                 style={styles.container}
